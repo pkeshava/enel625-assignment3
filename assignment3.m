2f0047c4df5fa384510ecf9a6b4ee19a84843389
Pouyan <pouyank13@me.com>
2017-04-20 12:50:37 -0600

    Got it working I think. Used fmincon and added a constraint on the DC offset, b, so mean of the noise came close to 0

--- assignment3.m
+++ assignment3.m
@@ -28,9 +28,16 @@ hold on
 y_ad_gamma = fmodel(b);
 plot(X,y_ad_gamma);
 hold off
+
+% Calculating MSE
+y_gamma_diff = y_ad_gamma-Y;
+y_gamma_diff_squared = y_gamma_diff.^2;
+MSE_gamma = sum(y_gamma_diff_squared)/size(Y,2);
 %% Normal
 
 clc
+clear all
+close all
 
 load('XYdata.mat');
 global x ;
@@ -43,10 +50,18 @@ y = Y;
 % Now I need the log-liklihood function to maximize
 
 b0 = [3 2 2 4 4];
-options = optimset('LargeScale', 'off', 'HessUpdate', 'dfp');
+%options = optimset('LargeScale', 'off', 'HessUpdate', 'bfgs');
 %[b, fval, flag, output] = fminunc(@flikelihoodnormal, b0, options);
-[b, fval, flag, output] = fminsearch(@flikelihoodnormal, b0, options);
-%phat = mle(Y,'nloglf',@flikelihoodnormal,'start',b0);
+
+options = optimset('LargeScale', 'off', 'HessUpdate', 'bfgs');
+lb = [-Inf, 11.75,-Inf,-Inf,-Inf];
+ub = [];
+
+[b, fval, flag, output] = fmincon(@flikelihoodnormal, b0, [],[],[],[],lb,ub);
+
+%options = optimset('LargeScale', 'off', 'HessUpdate', 'dfp', 'MaxFunEvals', 100000,'MaxIter', 100000);
+%[b, fval, flag, output] = fminsearch(@flikelihoodnormal, b0, options);
+
 
 a_normal = b(1);
 b_normal = b(2);
@@ -54,12 +69,21 @@ c_normal = b(3);
 mu_normal = b(4);
 var_normal = b(5);
 
-
+figure;
 plot(X,Y);
 hold on 
 y_ad_normal = fmodel(b);
 plot(X,y_ad_normal);
 hold off
+figure;
+xn = -50:0.5:50;
+norm = normpdf(xn,mu_normal,sqrt(var_normal));
+plot(xn,norm);
+
+% Calculating MSE
+y_norm_diff = Y-y_ad_normal;
+y_norm_diff_squared = y_norm_diff.^2;
+MSE_normal = sum(y_norm_diff_squared)/size(Y,2);
 %%
 
 clc
@@ -88,7 +112,13 @@ hold on
 y_ad_chi = fmodel(b);
 plot(X,y_ad_chi);
 hold off
-%%
+
+% Calculating MSE
+y_chi_diff = Y-y_ad_chi;
+y_chi_diff_squared = y_chi_diff.^2;
+MSE_chi = sum(y_chi_diff_squared)/size(Y,2);
+
+%% Plotting
 plot(X,Y);
 hold on
 plot(X,y_ad_chi);
@@ -96,3 +126,4 @@ plot(X,y_ad_normal);
 plot(X,y_ad_gamma);
 hold off
 legend('Original','Chi', 'Normal', 'Gamma');
+
